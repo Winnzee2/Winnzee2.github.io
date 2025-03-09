f62317fda92229c89437ab8aa4283bc2d8a2107b
Winnzee2 <winniezheng0409@sina.com>
2025-03-10 01:14:04 +0800

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Winnzee2.github.io',
+  base: '/',
+  integrations: [tailwind()],
+});
